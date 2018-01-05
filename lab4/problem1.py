ac3cd3da6236b290c28d348b409da3f6dd271851
MAD3RO <markohuljak555@gmail.com>
2018-01-06 00:18:38 +0100

    Zadaci sa labosa

--- lab4/problem1.py
+++ lab4/problem1.py
@@ -1,69 +1,16 @@
-# import numpy as np
-# import cv2
-
-# def invert_image(img):
-#     out_img = 255-img
-#     return out_img
-
-# image_path = "Images/airplane.bmp"
-
-# img = cv2.imread(image_path, cv2.IMREAD_GRAYSCALE)
-# imgg = cv2.imread(image_path, cv2.IMREAD_COLOR)
-
-# inv_img = invert_image(img)
-# inv_imgg = invert_image(imgg)
-# cv2.imshow("Invert",inv_img)
-# cv2.imshow("Original",img)
-# cv2.imshow("Original_color",imgg)
-# cv2.imshow("Invert_original",inv_imgg)
-# cv2.waitKey()
-# cv2.destroyAllWindows()
- 
-#zad2
+# Create a program which will load all images from images directory in grayscale 
+# perform the invert operation and 
+# save the images to problem1/imagename_invert.png.
 
 import numpy as np
 import cv2
 
-def invert_image(img):
-    out_img = 255-img
-    return out_img
+images = ["airplane.bmp", "baboon.bmp", "barbara.bmp", "barbara.pgm", "boats.bmp",
+          "BoatsColor.bmp", "dioslike.jpg", "goldhill.bmp", "hp_logo.jpg", 
+          "lenna.bmp", "pepper.bmp", "pero.jpg"]
 
-images_path = ["Images/airplane.bmp", 
-                "Images/baboon.bmp",
-                "Images/barbara.pgm",
-                "Images/goldhill.bmp",
-                "Images/pepper.bmp",
-                "Images/lenna.bmp"]
+for i in images:
+    img = cv2.imread("images/"+i, 0) # 0 = grayscale
+    inv = 255 - img
+    cv2.imwrite("problem1/"+i.split('.')[0]+"_invert.png", inv)
 
-# - perform the threshold operation with the parameter values of `63`, `127`, `191`
-# - save the images to `problem2/imagename_parametervalue_thresh.png`
-for image_path in images_path:
-    img = cv2.imread(image_path, cv2.IMREAD_COLOR)
-    inv_img = invert_image(img)
-    naziv_slike = image_path[7:-4]
-    cv2.imwrite("problem1/"+naziv_slike+".png",inv_img)
-    # for i in img:
-    #     for j in img:
-    #         if j[0]>63:
-    #             j[0]=63
-    thres1 = img[:,:,0] > 63
-    img[thres1,0]=63
-    thres2 = img[:,:,0] > 127
-    thres3 = img[:,:,0] > 191
-    img63=img[thres1,0]=63
-    img127=img[thres2,0]=127
-    img191=img[thres3,0]=191
-
-            
-    cv2.imshow("Threshold63",img)
-    # cv2.imshow("Threshold127",img127)
-    # cv2.imshow("Threshold191",img191)
-    cv2.waitKey()
-    cv2.destroyAllWindows()
-
-    
-
-
-
-
- 
